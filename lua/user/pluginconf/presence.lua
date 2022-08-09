5c9e43e3ac3bd698f431938054747aa515466d00
milindmadhukar <m.milind15@gmail.com>
2022-08-09 16:08:36 +0530

    Changed presence

--- lua/user/pluginconf/presence.lua
+++ lua/user/pluginconf/presence.lua
@@ -6,11 +6,11 @@ end
 presence:setup({
     -- General options
     auto_update         = true,                       -- Update activity based on autocmd events (if `false`, map or manually execute `:lua package.loaded.presence:update()`)
-    neovim_image_text   = "and it goes something like", -- Text displayed when hovered over the Neovim image
+    neovim_image_text   = "✚✖", -- Text displayed when hovered over the Neovim image
     main_image          = "file",                   -- Main image display (either "neovim" or "file")
     client_id           = "793271441293967371",       -- Use your own Discord application client id (not recommended)
     log_level           = nil,                        -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
-    debounce_timeout    = 10,                         -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
+    debounce_timeout    = 20,                         -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
     enable_line_number  = false,                      -- Displays the current line number instead of the current project
     blacklist           = {},                         -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
     buttons             = true,                       -- Configure Rich Presence button(s), either a boolean to enable/disable, a static table (`{{ label = "<label>", url = "<url>" }, ...}`, or a function(buffer: string, repo_url: string|nil): table)
