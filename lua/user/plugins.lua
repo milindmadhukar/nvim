3275943fdb3dec4c3aef27e6494cdfc797f7bd15
milindmadhukar <m.milind15@gmail.com>
2022-08-18 14:21:12 +0530

    load em plugins when saved

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -15,13 +15,13 @@ if fn.empty(fn.glob(install_path)) > 0 then
 	vim.cmd([[packadd packer.nvim]])
 end
 
--- Autocommand that reloads neovim whenever you save the plugins.lua file
--- vim.cmd [[
---  augroup packer_user_config
---    autocmd!
---    autocmd BufWritePost plugins.lua source <afile> | PackerSync
---  augroup end
--- ]]
+--[[ Autocommand that reloads neovim whenever you save the plugins.lua file ]]
+vim.cmd [[
+ augroup packer_user_config
+   autocmd!
+   autocmd BufWritePost plugins.lua source <afile> | PackerSync
+ augroup end
+]]
 
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
@@ -140,7 +140,6 @@ return packer.startup(function(use)
 		setup = function()
 			vim.g.mkdp_filetypes = { "markdown" }
 		end,
-		ft = { "markdown" },
 	})
 
 	use({
