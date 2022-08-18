2fe076c7923118a864f0c52d13d656d5d4bb3206
milindmadhukar <m.milind15@gmail.com>
2022-08-18 17:35:02 +0530

    transparency i see

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,29 +1,32 @@
 local colorscheme = "tokyonight"
 
+vim.api.nvim_create_autocmd({ "ColorScheme" }, {
+	callback = function()
+		vim.cmd([[
+      hi Normal ctermbg=none guibg=none
+      hi SignColumn ctermbg=none guibg=none
+      hi NormalNC ctermbg=none guibg=none
+      hi MsgArea ctermbg=none guibg=none
+      hi NvimTreeNormal ctermbg=none guibg=none
+      hi NvimTreeNormalNC ctermbg=none guibg=none
+      hi EndOfBuffer ctermbg=none guibg=none
+      hi LineNr guibg=none ctermbg=none
+      hi Folded guibg=none ctermbg=none
+      hi FoldColumn guibg=none ctermbg=none
+      hi VertSplit guibg=none ctermbg=none
+      hi NormalFloat guibg=none ctermbg=none
+      hi FloatBorder guibg=none ctermbg=none
+      hi Pmenu guibg=none ctermbg=none
+      hi Directory guibg=none ctermbg=none
+      hi Debug guibg=none ctermbg=none
+      hi NonText guibg=none ctermbg=none
+    ]])
+	end,
+})
+
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-  return
+	return
 end
 
-vim.cmd [[
-hi Normal ctermbg=none guibg=none
-hi SignColumn ctermbg=none guibg=none
-hi NormalNC ctermbg=none guibg=none
-hi MsgArea ctermbg=none guibg=none
-hi NvimTreeNormal ctermbg=none guibg=none
-hi NvimTreeNormalNC ctermbg=none guibg=none
-hi EndOfBuffer ctermbg=none guibg=none
-hi LineNr guibg=none ctermbg=none
-hi Folded guibg=none ctermbg=none
-hi FoldColumn guibg=none ctermbg=none
-hi VertSplit guibg=none ctermbg=none
-hi NormalFloat guibg=none ctermbg=none
-hi FloatBorder guibg=none ctermbg=none
-hi Pmenu guibg=none ctermbg=none
-hi Directory guibg=none ctermbg=none
-hi Debug guibg=none ctermbg=none
-hi NonText guibg=none ctermbg=none
-]]
-
-
-vim.cmd "let &fcs='eob: '"
+vim.cmd("let &fcs='eob: '")
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -16,12 +16,12 @@ if fn.empty(fn.glob(install_path)) > 0 then
 end
 
 --[[ Autocommand that reloads neovim whenever you save the plugins.lua file ]]
-vim.cmd [[
+vim.cmd([[
  augroup packer_user_config
    autocmd!
    autocmd BufWritePost plugins.lua source <afile> | PackerSync
  augroup end
-]]
+]])
 
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
@@ -59,7 +59,7 @@ return packer.startup(function(use)
 	use("folke/which-key.nvim")
 
 	-- Colorschemes
-	use({ "folke/tokyonight.nvim" })
+	use("folke/tokyonight.nvim")
 	use("lunarvim/darkplus.nvim")
 	use("Shadorain/shadotheme")
 	use("LunarVim/synthwave84.nvim")
