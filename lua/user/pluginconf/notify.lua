f05f3a379ef2e18cf555fa3d254e5f148768bf16
Milind <m.milind15@gmail.com>
2023-02-12 18:22:03 +0530

    Update to hide clangd warning

--- lua/user/pluginconf/notify.lua
+++ lua/user/pluginconf/notify.lua
@@ -4,36 +4,36 @@ if not status_ok then
 end
 
 notify.setup {
-  -- Animation style (see below for details)
-  stages = "fade_in_slide_out",
+    -- Animation style (see below for details)
+    stages = "fade_in_slide_out",
 
-  -- Function called when a new window is opened, use for changing win settings/config
-  on_open = nil,
+    -- Function called when a new window is opened, use for changing win settings/config
+    on_open = nil,
 
-  -- Function called when a window is closed
-  on_close = nil,
+    -- Function called when a window is closed
+    on_close = nil,
 
-  -- Render function for notifications. See notify-render()
-  render = "default",
+    -- Render function for notifications. See notify-render()
+    render = "default",
 
-  -- Default timeout for notifications
-  timeout = 175,
+    -- Default timeout for notifications
+    timeout = 175,
 
-  -- For stages that change opacity this is treated as the highlight behind the window
-  -- Set this to either a highlight group or an RGB hex value e.g. "#000000"
-  background_colour = "#000000",
+    -- For stages that change opacity this is treated as the highlight behind the window
+    -- Set this to either a highlight group or an RGB hex value e.g. "#000000"
+    background_colour = "#000000",
 
-  -- Minimum width for notification windows
-  minimum_width = 10,
+    -- Minimum width for notification windows
+    minimum_width = 10,
 
-  -- Icons for the different levels
-  icons = {
-    ERROR = " ",
-    WARN = " ",
-    INFO = "",
-    DEBUG = " ",
-    TRACE = " ",
-  },
+    -- Icons for the different levels
+    icons = {
+        ERROR = " ",
+        WARN = " ",
+        INFO = "",
+        DEBUG = " ",
+        TRACE = " ",
+    },
 }
 
 vim.notify = notify
@@ -44,6 +44,9 @@ vim.notify = function(msg, ...)
     return
   end
   if msg:match "method textDocument" then
+    return
+  end
+  if msg:match "warning: multiple different client offset_encodings" then
     return
   end
 
