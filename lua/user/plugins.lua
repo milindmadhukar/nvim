deba89b54bfb59b78cc06f3ac21d1bea5798f1d5
Milind <m.milind15@gmail.com>
2023-06-22 00:49:13 +0530

    cloack and hardtime

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -446,6 +446,49 @@ local plugins = {
 		end,
 	},
 
+	{
+		"laytan/cloak.nvim",
+		config = function()
+			require("user.pluginconf.cloak")
+		end,
+	},
+
+	{
+		"m4xshen/hardtime.nvim",
+		opts = {
+			max_time = 500,
+			max_count = 5,
+			disable_mouse = true,
+			hint = true,
+			allow_different_key = false,
+			resetting_keys = {
+				"0",
+				"1",
+				"2",
+				"3",
+				"4",
+				"5",
+				"6",
+				"7",
+				"8",
+				"9",
+				"c",
+				"C",
+				"d",
+				"x",
+				"X",
+				"y",
+				"Y",
+				"p",
+				"P",
+			},
+			restricted_keys = { "h", "j", "k", "l", "-", "+", "gj", "gk" },
+			hint_keys = { "k", "j", "^", "$", "a", "i", "d", "y", "c", "l" },
+			disabled_keys = { "<UP>", "<DOWN>", "<LEFT>", "<RIGHT>" },
+			disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason" },
+		},
+	},
+
 	-- use {
 	--   'KadoBOT/nvim-spotify',
 	--   dependencies = 'nvim-telescope/telescope.nvim',
