55f81e1afaa7abe6ae96186844d517c8df9f0d8f
Milind <m.milind15@gmail.com>
2025-12-08 03:04:14 +0530

    auto import autocmd for go

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -70,6 +70,30 @@ vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 	end,
 })
 
+-- Organize imports on save for Go files
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*.go",
+	callback = function()
+		local clients = vim.lsp.get_clients({ bufnr = 0, name = "gopls" })
+		if #clients == 0 then
+			return
+		end
+		local client = clients[1]
+		local params = vim.lsp.util.make_range_params(nil, client.offset_encoding)
+		params.context = { only = { "source.organizeImports" } }
+		local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 1000)
+		for _, res in pairs(result or {}) do
+			for _, r in pairs(res.result or {}) do
+				if r.edit then
+					vim.lsp.util.apply_workspace_edit(r.edit, client.offset_encoding)
+				else
+					vim.lsp.buf.execute_command(r.command)
+				end
+			end
+		end
+	end,
+})
+
 vim.api.nvim_create_autocmd({ "VimEnter" }, {
 	callback = function()
 		vim.cmd("hi link illuminatedWord LspReferenceText")
